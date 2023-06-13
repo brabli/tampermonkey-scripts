4a45944d3ab22bb3c370a239955981a1d16d16a7
Brabli <67018167+Brabli@users.noreply.github.com>
2023-06-13 23:31:09 +0100

    Update script to 1.1.0

--- youtube-shorts-hider/script.js
+++ youtube-shorts-hider/script.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         YouTube Shorts Hider
-// @version      1.0.0
+// @version      1.1.0
 // @description  Removes YouTube shorts from my YouTube subscription list
 // @author       brabli
-// @match        https://www.youtube.com/feed/subscriptions
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @source       https://github.com/brabli/tampermonkey-scripts
 // ==/UserScript==
@@ -24,13 +24,25 @@ function findAndRemoveShorts() {
     const shorts = findShorts();
     logNumberOfShorts(shorts);
     removeElements(shorts);
+
+    const reelShelves = findReelShelves();
+    removeElements(reelShelves);
 }
 
 function findShorts() {
     const shortIcons = document.querySelectorAll('#overlays > [overlay-style="SHORTS"]');
-    const shorts = Array.from(shortIcons).map(icon => icon.closest('ytd-grid-video-renderer'));
+
+    const shorts = Array
+        .from(shortIcons)
+        .map(icon => icon.closest('ytd-grid-video-renderer') ?? icon.closest('ytd-rich-item-renderer'))
+        .filter(short => null !== short)
+    ;
 
     return shorts;
+}
+
+function findReelShelves() {
+    return Array.from(document.querySelectorAll('ytd-reel-shelf-renderer'));
 }
 
 function logNumberOfShorts(shorts) {
